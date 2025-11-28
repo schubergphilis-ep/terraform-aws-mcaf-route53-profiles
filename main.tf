c3a9b46660eb7101b3941938a06db8f1a105f205
Keith Segovia <ksegovia@schubergphilis.com>
2025-11-28 10:42:37 +0100

    feat!: Add region support

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_route53profiles_profile" "default" {
-  name = var.name
+  name   = var.name
+  region = var.region
 }
 
 resource "aws_route53profiles_resource_association" "default" {
@@ -7,5 +8,6 @@ resource "aws_route53profiles_resource_association" "default" {
 
   name         = each.key
   profile_id   = aws_route53profiles_profile.default.id
+  region       = var.region
   resource_arn = each.value
 }
